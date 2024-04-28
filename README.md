67c61db4802221c55481a29f4082f24a43a8fe23
Romte1 <ivan_ni224@hotmail.com>
2024-04-28 06:05:04 -0400

    Several changes made for webpack configuration

--- README.md
+++
@@ -1,2 +0,0 @@
-# todo-list
-TOP todo-list
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-    }),
-],
-};
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ]
+}
