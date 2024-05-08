fe648e87517c38cf3e0e9de32ab67e6a141d6733
Romte1 <ivan_ni224@hotmail.com>
2024-05-08 07:11:03 -0400

    Adding more text to the nav, no functions added yet

--- src/todoList.js
+++ src/todoList.js
@@ -2,9 +2,11 @@ function home () {
     const content = document.querySelector('.content');
     const navigator = document.querySelector('.nav');
     const navMenuTitle = document.createElement('h2');
+    const navAddNewProjectDiv = document.createElement('div');
 
     navMenuTitle.textContent = 'Tasks';
     navigator.appendChild(navMenuTitle);
+    navigator.appendChild(navAddNewProjectDiv);
 
 
 }
