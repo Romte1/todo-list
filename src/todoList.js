228bccd1ee9036f3526fe83734b0d8a74fdc4037
Romte1 <ivan_ni224@hotmail.com>
2024-05-11 06:05:53 -0400

    Adding features to the nav, no functionalities yet, only styling

--- src/todoList.js
+++ src/todoList.js
@@ -3,8 +3,12 @@ function home () {
     const navigator = document.querySelector('.nav');
     const navMenuTitle = document.createElement('h2');
     const navAddNewProjectDiv = document.createElement('div');
+    const navAddNewPorjectP = document.createElement('p');
 
     navMenuTitle.textContent = 'Tasks';
+    navAddNewPorjectP.textContent = 'Add New Project';
+
+    navAddNewProjectDiv.appendChild(navAddNewPorjectP);
     navigator.appendChild(navMenuTitle);
     navigator.appendChild(navAddNewProjectDiv);
 
