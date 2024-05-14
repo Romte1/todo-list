e754a7b27f6e21dc79f6fb75571b025bb8f27c10
Romte1 <ivan_ni224@hotmail.com>
2024-05-14 06:16:23 -0400

    Working on nav menu bar, adding submenus and such

--- src/todoList.js
+++ src/todoList.js
@@ -1,15 +1,23 @@
 function home () {
     const content = document.querySelector('.content');
     const navigator = document.querySelector('.nav');
-    const navMenuTitle = document.createElement('h2');
+    const navMenuTitle1 = document.createElement('h2');
+    const navTodayDiv = document.createElement('div');
+    const navTodayP = document.createElement('p');
     const navAddNewProjectDiv = document.createElement('div');
     const navAddNewPorjectP = document.createElement('p');
 
-    navMenuTitle.textContent = 'Tasks';
+    navMenuTitle1.textContent = 'Tasks';
+
+    navTodayP.textContent = 'Today';
+
     navAddNewPorjectP.textContent = 'Add New Project';
 
+    navTodayDiv.appendChild(navTodayP);
     navAddNewProjectDiv.appendChild(navAddNewPorjectP);
-    navigator.appendChild(navMenuTitle);
+
+    navigator.appendChild(navMenuTitle1);
+    navigator.appendChild(navTodayDiv);
     navigator.appendChild(navAddNewProjectDiv);
 
 
