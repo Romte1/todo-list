a15c3b6d260eeb26b85bd0c50ef9e0950f84713a
Romte1 <ivan_ni224@hotmail.com>
2024-05-15 13:19:57 -0400

    Added more submenus to the nav bar

--- src/todoList.js
+++ src/todoList.js
@@ -3,21 +3,26 @@ function home () {
     const navigator = document.querySelector('.nav');
     const navMenuTitle1 = document.createElement('h2');
     const navTodayDiv = document.createElement('div');
+    const navThisWeekDiv = document.createElement('div');
     const navTodayP = document.createElement('p');
+    const navThisWeekP = document.createElement('p');
     const navAddNewProjectDiv = document.createElement('div');
     const navAddNewPorjectP = document.createElement('p');
 
     navMenuTitle1.textContent = 'Tasks';
 
     navTodayP.textContent = 'Today';
+    navThisWeekP.textContent = 'This Week';
 
     navAddNewPorjectP.textContent = 'Add New Project';
 
     navTodayDiv.appendChild(navTodayP);
+    navThisWeekDiv.appendChild(navThisWeekP);
     navAddNewProjectDiv.appendChild(navAddNewPorjectP);
 
     navigator.appendChild(navMenuTitle1);
     navigator.appendChild(navTodayDiv);
+    navigator.appendChild(navThisWeekDiv);
     navigator.appendChild(navAddNewProjectDiv);
 
 
