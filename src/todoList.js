28a57ee603e472d66f06bae440e0a608f42a0e97
Romte1 <ivan_ni224@hotmail.com>
2024-05-16 11:39:35 -0400

    More work done in the nav bar section, new submenu for all tasks, no functionality yet

--- src/todoList.js
+++ src/todoList.js
@@ -4,8 +4,10 @@ function home () {
     const navMenuTitle1 = document.createElement('h2');
     const navTodayDiv = document.createElement('div');
     const navThisWeekDiv = document.createElement('div');
+    const navAllTasksDiv = document.createElement('div');
     const navTodayP = document.createElement('p');
     const navThisWeekP = document.createElement('p');
+    const navAllTasksP = document.createElement('p');
     const navAddNewProjectDiv = document.createElement('div');
     const navAddNewPorjectP = document.createElement('p');
 
@@ -13,16 +15,23 @@ function home () {
 
     navTodayP.textContent = 'Today';
     navThisWeekP.textContent = 'This Week';
+    navAllTasksP.textContent = 'All Tasks';
 
     navAddNewPorjectP.textContent = 'Add New Project';
 
     navTodayDiv.appendChild(navTodayP);
     navThisWeekDiv.appendChild(navThisWeekP);
+    navAllTasksDiv.appendChild(navAllTasksP);
+
     navAddNewProjectDiv.appendChild(navAddNewPorjectP);
+    
 
     navigator.appendChild(navMenuTitle1);
+    
     navigator.appendChild(navTodayDiv);
     navigator.appendChild(navThisWeekDiv);
+    navigator.appendChild(navAllTasksDiv);
+
     navigator.appendChild(navAddNewProjectDiv);
 
 
