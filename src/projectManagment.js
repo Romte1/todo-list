682fec6576069ae2d65983a91e83a18533c4fbde
Romte1 <ivan_ni224@hotmail.com>
2024-05-21 00:09:54 -0400

    Added a modal that will let the user add a new project to the list, not workig yet, needs styling too

--- src/projectManagment.js
+++ src/projectManagment.js
@@ -1,4 +1,29 @@
 function newProject() {
+    const body = document.querySelector('body');
+    const newProjectModal = document.createElement('dialog');
+    const newProjectP = document.createElement('p');
+    const newProjectLbl = document.createElement('label');
+    const newProjectIpt = document.createElement('input');
+
+    newProjectP.textContent = 'New Project';
+    newProjectLbl.textContent = 'Name: ';
+
+    newProjectLbl.setAttribute('for', 'Name');
+
+    newProjectIpt.setAttribute('type', 'text');
+    newProjectIpt.setAttribute('id', 'Name');
+    newProjectIpt.setAttribute('name', 'Name');
+
+    newProjectModal.setAttribute('class', 'modal');
+
+    newProjectModal.appendChild(newProjectP);
+    newProjectModal.appendChild(newProjectLbl);
+    newProjectModal.appendChild(newProjectIpt);
+
+    body.appendChild(newProjectModal);
+
+    newProjectModal.showModal();
+
     alert('hola');
 }
 
