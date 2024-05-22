85f8ca8b1808ee5cff300bd8b3a9144e3e049f99
Romte1 <ivan_ni224@hotmail.com>
2024-05-22 00:27:12 -0400

    More changes made, added a button to create the project in the modal, no function added yet

--- src/projectManagment.js
+++ src/projectManagment.js
@@ -4,9 +4,11 @@ function newProject() {
     const newProjectP = document.createElement('p');
     const newProjectLbl = document.createElement('label');
     const newProjectIpt = document.createElement('input');
+    const newProjectBtn = document.createElement('button');
 
     newProjectP.textContent = 'New Project';
     newProjectLbl.textContent = 'Name: ';
+    newProjectBtn.textContent = 'Create!';
 
     newProjectLbl.setAttribute('for', 'Name');
 
@@ -19,12 +21,12 @@ function newProject() {
     newProjectModal.appendChild(newProjectP);
     newProjectModal.appendChild(newProjectLbl);
     newProjectModal.appendChild(newProjectIpt);
+    newProjectModal.appendChild(newProjectBtn);
 
     body.appendChild(newProjectModal);
 
     newProjectModal.showModal();
 
-    alert('hola');
 }
 
 export default newProject;
