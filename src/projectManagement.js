1a53cbf5939d98f4e556711573a5d133464021a8
Romte1 <ivan_ni224@hotmail.com>
2024-06-17 01:24:34 -0400

    Added a new function that will  do the deletion part of projects

--- src/projectManagement.js
+++ src/projectManagement.js
@@ -1,65 +1,65 @@
     let Projects = [];
     
     function newProject() {
-    const body = document.querySelector('body');
-    const newProjectModal = document.createElement('dialog');
-    const newProjectP = document.createElement('p');
-    const newProjectLbl = document.createElement('label');
-    const newProjectIpt = document.createElement('input');
-    const newProjectMsg = document.createElement('label');
-    const newProjectBtn = document.createElement('button');
+        const body = document.querySelector('body');
+        const newProjectModal = document.createElement('dialog');
+        const newProjectP = document.createElement('p');
+        const newProjectLbl = document.createElement('label');
+        const newProjectIpt = document.createElement('input');
+        const newProjectMsg = document.createElement('label');
+        const newProjectBtn = document.createElement('button');
 
-    newProjectP.textContent = 'New Project';
-    newProjectLbl.textContent = 'Name: ';
-    newProjectBtn.textContent = 'Create!';
+        newProjectP.textContent = 'New Project';
+        newProjectLbl.textContent = 'Name: ';
+        newProjectBtn.textContent = 'Create!';
 
-    newProjectLbl.setAttribute('for', 'Name');
+        newProjectLbl.setAttribute('for', 'Name');
 
-    newProjectIpt.setAttribute('type', 'text');
-    newProjectIpt.setAttribute('id', 'Name');
-    newProjectIpt.setAttribute('name', 'Name');
-    newProjectIpt.setAttribute('maxlength', '15');
+        newProjectIpt.setAttribute('type', 'text');
+        newProjectIpt.setAttribute('id', 'Name');
+        newProjectIpt.setAttribute('name', 'Name');
+        newProjectIpt.setAttribute('maxlength', '15');
 
-    addEventListener ('click' , ({target}) => {
+        addEventListener ('click' , ({target}) => {
 
-        if (target === newProjectBtn && newProjectIpt.value !== '' ) {
+            if (target === newProjectBtn && newProjectIpt.value !== '' ) {
 
-            for (let i=0; i<Projects.length; i++) {
-                if (Projects[i].name.toLowerCase() === newProjectIpt.value.toLowerCase()) {
-                    newProjectMsg.setAttribute('class', 'error');
-                    newProjectMsg.textContent = 'Projects must have an unique name!';
-                    console.log(Projects.length);
-                    return;
+                for (let i=0; i<Projects.length; i++) {
+                    if (Projects[i].name.toLowerCase() === newProjectIpt.value.toLowerCase()) {
+                        newProjectMsg.setAttribute('class', 'error');
+                        newProjectMsg.textContent = 'Projects must have an unique name!';
+                        console.log(Projects.length);
+                        return;
+                    }
                 }
+                var project = new Project(newProjectIpt.value);
+                Projects.push(project);
+                console.log(Projects.length);
+                newProjectModal.remove();
+                updateProjectsList();
             }
-            var project = new Project(newProjectIpt.value);
-            Projects.push(project);
-            console.log(Projects.length);
-            newProjectModal.remove();
-            updateProjectsList();
-        }
-    });
+        });
 
 
-    // removes modal when pressin escape key
-    document.addEventListener('keyup',function(e){
-        if (e.key === "Escape") { 
-            
-            newProjectModal.remove();
-        }
-    });
+        // removes modal when pressin escape key
+        document.addEventListener('keyup',function(e){
+            if (e.key === "Escape") { 
+                
+                newProjectModal.remove();
+            }
+        });
 
-    newProjectModal.setAttribute('class', 'modal');
+        newProjectModal.setAttribute('class', 'modal');
 
-    newProjectModal.appendChild(newProjectP);
-    newProjectModal.appendChild(newProjectLbl);
-    newProjectModal.appendChild(newProjectIpt);
-    newProjectModal.appendChild(newProjectMsg);
-    newProjectModal.appendChild(newProjectBtn);
+        newProjectModal.appendChild(newProjectP);
+        newProjectModal.appendChild(newProjectLbl);
+        newProjectModal.appendChild(newProjectIpt);
+        newProjectModal.appendChild(newProjectMsg);
+        newProjectModal.appendChild(newProjectBtn);
 
-    body.appendChild(newProjectModal);
+        body.appendChild(newProjectModal);
 
-    newProjectModal.showModal();
+        newProjectModal.showModal();
 
     }
 
@@ -92,10 +92,9 @@
 
             projectDel.addEventListener('click', () => {
                 let name = Projects[i].name;
-                console.log(Projects[i].name);
-                Projects = Projects.filter(item => item.name !== name);
-                updateProjectsList();
-                console.log(Projects);
+                console.log(name);
+                deleteProject(name);
+
             })
             
             projectDiv.appendChild(projectP);
@@ -105,6 +104,12 @@
             navProjects.appendChild(projectDiv);
         }
 
+    }
+
+    function deleteProject(name) {
+        Projects = Projects.filter(item => item.name !== name);
+        updateProjectsList();
+        console.log(Projects);
     }
 
     class Project {
