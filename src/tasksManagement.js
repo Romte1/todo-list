582e352f7cb5066e958c50dcf01be019fc4f5f68
Romte1 <ivan_ni224@hotmail.com>
2024-07-18 00:48:07 -0400

    Added more fields to new task dialog

--- src/tasksManagement.js
+++ src/tasksManagement.js
@@ -6,6 +6,9 @@ let tasks = [];
         const taskNameDiv = document.createElement('div');
         const taskNameLbl = document.createElement('label');
         const taskNameIpt = document.createElement('input');
+        const taskDescriptionDiv = document.createElement('div');
+        const taskDescriptionLbl = document.createElement('label');
+        const taskDescriptionIpt = document.createElement('input');    
 
         newTaskModal.setAttribute('class', 'modal-new-task');
 
@@ -14,8 +17,16 @@ let tasks = [];
         taskNameLbl.textContent = 'Name:';
         taskNameLbl.setAttribute('for', 'Name');
 
+        taskDescriptionLbl.textContent = 'Description:';
+        taskDescriptionLbl.setAttribute('for','Description');
+
         taskNameIpt.setAttribute('id', 'Name');
         taskNameIpt.setAttribute('name', 'Name');
+
+        taskDescriptionIpt.setAttribute('id', 'Description');
+        taskDescriptionIpt.setAttribute('name', 'Description');
+
+
 
         // removes modal when pressin escape key
         document.addEventListener('keyup',function(e){
@@ -28,8 +39,12 @@ let tasks = [];
         taskNameDiv.appendChild(taskNameLbl);
         taskNameDiv.appendChild(taskNameIpt);
 
+        taskDescriptionDiv.appendChild(taskDescriptionLbl);
+        taskDescriptionDiv.appendChild(taskDescriptionIpt);
+
         newTaskModal.appendChild(newTaskP);
         newTaskModal.appendChild(taskNameDiv);
+        newTaskModal.appendChild(taskDescriptionDiv);
 
         body.appendChild(newTaskModal);
 
